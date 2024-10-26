12c09a773088b70850ed1f87aaa12362f5727bd0
Ravi Ranjan Prakash <96382607+raviranjanprakash@users.noreply.github.com>
2024-10-26 08:09:47 +0530

    Update host-assets-base-url.php

--- inc/host-assets-base-url.php
+++ inc/host-assets-base-url.php
@@ -1,6 +1,6 @@
 <?php
-// define("HOST_BASE_URL", 'https://' . $_SERVER["HTTP_HOST"] . "/");
-define("HOST_BASE_URL", 'http://' . $_SERVER["HTTP_HOST"] . "/companywebsite/");
-// define("ASSETS_BASE_URL", 'https://' . $_SERVER["HTTP_HOST"] . "/");
-define("ASSETS_BASE_URL", 'http://' . $_SERVER["HTTP_HOST"] . "/companywebsite/");
+define("HOST_BASE_URL", 'https://' . $_SERVER["HTTP_HOST"] . "/");
+// define("HOST_BASE_URL", 'http://' . $_SERVER["HTTP_HOST"] . "/companywebsite/");
+define("ASSETS_BASE_URL", 'https://' . $_SERVER["HTTP_HOST"] . "/");
+// define("ASSETS_BASE_URL", 'http://' . $_SERVER["HTTP_HOST"] . "/companywebsite/");
 ?>
